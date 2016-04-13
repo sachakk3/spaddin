044ab40b778120a701f993db6d5f61df9493abe6
sacha_kk3 <sacha_kk3@list.ru>
2016-04-13 12:37:55 +0300

    Добавлен командлет для обновления проектов

---
+++ Projects/Algosmart.SharePoint.TimeSheetReceiver/Algosmart.SharePoint.Cmdlets/Cmdlets/UpdateProjects.cs
@@ -0,0 +1,140 @@
+﻿using Algosmart.SharePoint.Cmdlets.Code;
+using Algosmart.SharePoint.TimeSheetReceiverWeb.Code;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algosmart.SharePoint.Cmdlets
+{
+    [Cmdlet("Update", "Projects")]
+    public class UpdateProjects : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string ProjectSiteURL { get; set; }
+        [Parameter(Mandatory = true)]
+        public string Login { get; set; }
+        [Parameter(Mandatory = true)]
+        public string Password { get; set; }  
+
+        /// <summary>
+        /// Задание параметров и запуск экспорта
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            Update();
+        }
+        public void Update()
+        {
+            ClientContext clientContext = Code.Helper.GetO365Context(this.ProjectSiteURL, this.Login, this.Password);
+
+            string listProjectsTitle = "Проекты";
+            Console.WriteLine(string.Format("Получение списка элементов", listProjectsTitle));
+            List timeSheets = clientContext.Web.Lists.GetByTitle(listProjectsTitle);
+            CamlQuery query = CamlQuery.CreateAllItemsQuery();
+            ListItemCollection items = timeSheets.GetItems(query);
+            clientContext.Load(items);
+            clientContext.ExecuteQuery();
+
+            foreach (ListItem item in items)
+            {
+
+                string projectInternalName = item[Constants.FIELDS_INTERNAL_NAME] + "";
+                if (!string.IsNullOrEmpty(projectInternalName))
+                {
+                    SetPermissions(clientContext, item, projectInternalName);
+                }
+            }
+        }
+        private static void SetPermissions(ClientContext clientContext, ListItem item,string projectInternalName)
+        {
+            FieldUserValue projectManager = item[Constants.FIELDS_PROJECTS_PM] as FieldUserValue;
+            FieldUserValue[] projectMembers = item[Constants.FIELDS_PROJECTS_USERS] as FieldUserValue[];
+
+
+            string prjPMGroupName = string.Format("{0}-PM", projectInternalName);
+            string prjTeamGroupName = string.Format("{0}-Team", projectInternalName);
+
+            Console.WriteLine("Получение всех групп");
+            GroupCollection groups = clientContext.Web.SiteGroups;
+            clientContext.Load(groups);
+            clientContext.ExecuteQuery();
+
+            Group groupOwner = groups.Where(g => g.Title.ToLower() == Constants.GROUPS_OWNER_TITLE.ToLower()).FirstOrDefault();
+            Group groupBoss = groups.Where(g => g.Title.ToLower() == Constants.GROUPS_BOSS_TITLE.ToLower()).FirstOrDefault();
+
+            Group groupHR = groups.Where(g => g.Title.ToLower() == Constants.GROUPS_HR_TITLE.ToLower()).FirstOrDefault();
+            Group groupFin = groups.Where(g => g.Title.ToLower() == Constants.GROUPS_Fin_TITLE.ToLower()).FirstOrDefault();
+            Group groupBackOfficePM = groups.Where(g => g.Title.ToLower() == Constants.GROUPS_PM_TITLE.ToLower()).FirstOrDefault();            
+
+            Group groupPM = groups.Where(g => g.Title.ToLower() == prjPMGroupName.ToLower()).FirstOrDefault();
+            Group groupTeam = groups.Where(g => g.Title.ToLower() == prjTeamGroupName.ToLower()).FirstOrDefault();
+
+            if (groupPM == null)
+            {
+                Console.WriteLine("Создание группы '{0}'", prjPMGroupName);
+                groupPM = TimeSheetReceiverWeb.Code.Helper.CreateGroup(clientContext, prjPMGroupName);
+                if (projectManager != null)
+                {
+                    User user = clientContext.Web.SiteUsers.GetById(projectManager.LookupId);
+                    groupPM.Users.AddUser(user);
+                }
+                else
+                {
+                    Console.WriteLine("Для проекта '{0}' не указан менеджер проекта", item["Title"]);
+                }
+            }
+            if (groupTeam == null)
+            {
+                Console.WriteLine("Создание группы '{0}'", prjTeamGroupName);
+                groupTeam = TimeSheetReceiverWeb.Code.Helper.CreateGroup(clientContext, prjTeamGroupName);
+                if (projectMembers != null)
+                {
+                    foreach (FieldUserValue member in projectMembers)
+                    {
+                        User user = clientContext.Web.SiteUsers.GetById(member.LookupId);
+                        groupTeam.Users.AddUser(user);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Для проекта '{0}' не указаны участники проекта", item["Title"]);
+                }
+            }            
+            SetPermissionsForProjectFolder(clientContext, item, groupPM, groupTeam, groupOwner, groupBoss, groupHR, groupFin, groupBackOfficePM);
+        }
+        private static void SetPermissionsForProjectFolder(ClientContext clientContext, ListItem item, Group groupPM, Group groupTeam, Group groupOwner, Group groupBoss, Group groupHR, Group groupFin, Group groupBackOfficePM)
+        {
+            RemoveAllRoleAssignments(clientContext, item);
+
+            RoleDefinitionBindingCollection collRoleFullControlDefinitionBinding = TimeSheetReceiverWeb.Code.Helper.GetRoleFullControl(clientContext);
+            item.RoleAssignments.Add(groupOwner, collRoleFullControlDefinitionBinding);
+
+            RoleDefinitionBindingCollection collRoleContributeDefinitionBinding = TimeSheetReceiverWeb.Code.Helper.GetRoleContribute(clientContext);
+            item.RoleAssignments.Add(groupBoss, collRoleContributeDefinitionBinding);
+            item.RoleAssignments.Add(groupPM, collRoleContributeDefinitionBinding);
+
+            RoleDefinitionBindingCollection collRoleReaderDefinitionBinding = TimeSheetReceiverWeb.Code.Helper.GetRoleReader(clientContext);
+            item.RoleAssignments.Add(groupHR, collRoleReaderDefinitionBinding);
+            item.RoleAssignments.Add(groupFin, collRoleReaderDefinitionBinding);
+            item.RoleAssignments.Add(groupBackOfficePM, collRoleReaderDefinitionBinding);
+            item.RoleAssignments.Add(groupTeam, collRoleReaderDefinitionBinding);
+            clientContext.ExecuteQuery();
+        }
+        private static void RemoveAllRoleAssignments(ClientContext clientContext, ListItem item)
+        {
+            item.BreakRoleInheritance(false, false);
+            RoleAssignmentCollection roleAssignments = item.RoleAssignments;
+            clientContext.Load(roleAssignments);
+            clientContext.ExecuteQuery();
+
+            for (int i = item.RoleAssignments.Count - 1; i >= 0; i--)
+            {
+                item.RoleAssignments[i].DeleteObject();
+            }
+        }
+    }
+}
---
+++ Projects/Algosmart.SharePoint.TimeSheetReceiver/Algosmart.SharePoint.Cmdlets/Code/Helper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algosmart.SharePoint.Cmdlets.Code
+{
+    public static class Helper
+    {
+        public static ClientContext GetO365Context(string url, string userName, string password)
+        {
+            ClientContext context = new ClientContext(url);
+            var passWord = new SecureString();
+            foreach (char c in password.ToCharArray()) passWord.AppendChar(c);
+            context.Credentials = new SharePointOnlineCredentials(userName, passWord);
+            var web = context.Web;
+            context.Load(web);
+            context.ExecuteQuery();
+            return context;
+        }
+    }
+}
